beed923b4c77a6c990bc55d6e1b8bb741d67a76c
Xavier Hazzard <xavier@hazzard.org.uk>
2021-08-10 18:31:14 +0100

    Added commands for gifs and for a shutdown from admin command.

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
+	"time"
 
 	embed "github.com/Clinet/discordgo-embed"
 	"github.com/bwmarrin/discordgo"
@@ -72,12 +74,60 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
 
-	if m.Content == "no" {
+	if strings.ToLower(m.Content) == "no" {
 		s.ChannelMessageSend(m.ChannelID, "Ha Get Rekt noob!")
 	}
 
-	if m.Content == "bruh" {
+	if strings.ToLower(m.Content) == "bruh" {
 		imgEmbed := embed.NewEmbed().SetImage("https://biographyhub.com/wp-content/uploads/2021/04/Arsenal-RL.jpg").MessageEmbed
 		s.ChannelMessageSendEmbed(m.ChannelID, imgEmbed)
 	}
+
+	if strings.EqualFold(m.Content, "Who's the best?") {
+		imgEmbed := embed.NewEmbed().SetTitle("Ya boi is da best, ya feel me!").SetImage("https://images2.minutemediacdn.com/image/upload/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/dataimagewebpbase64UklGRraHAABXRUJQVlA4IKqHAADwjwK-7debd14d7c6b7af842fbb2af6a2b701e.jpg").MessageEmbed
+		s.ChannelMessageSendEmbed(m.ChannelID, imgEmbed)
+	}
+
+	if strings.EqualFold(m.Content, "Kill me") {
+		imgEmbed := embed.NewEmbed().SetImage("https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/hostedimages/1459980594i/18682005.jpg").MessageEmbed
+		s.ChannelMessageSendEmbed(m.ChannelID, imgEmbed)
+	}
+
+	if strings.EqualFold(m.Content, "Arsenal Close") {
+		if isAdmin(&m.Author.ID) {
+			s.ChannelMessageSend(m.ChannelID, "Bot closing in 2 seconds")
+			time.Sleep(2 * time.Second)
+			panic("Program terminated by admin")
+
+		} else {
+			s.ChannelMessageSend(m.ChannelID, ("You don't have permission to use this command " + m.Author.Mention()))
+
+		}
+	}
+
+	if strings.ToLower(m.Content) == "arbb" {
+		s.ChannelMessageDelete(m.ChannelID, m.ID)
+		if isAdmin(&m.Author.ID) {
+
+			imgEmbed := embed.NewEmbed().SetImage("https://c.tenor.com/QaGZ50VlEPEAAAAM/think-about-it-use-your-brain.gif").MessageEmbed
+			s.ChannelMessageSendEmbed(m.ChannelID, imgEmbed)
+
+		} else {
+			imgEmbed := embed.NewEmbed().SetImage("https://www.sbs.com.au/guide/sites/sbs.com.au.guide/files/styles/body_image/public/brain_leaves.gif?itok=S_hKrg8V&mtime=1470115287").MessageEmbed
+			s.ChannelMessageSendEmbed(m.ChannelID, imgEmbed)
+		}
+
+	}
+	if strings.ToLower(m.Content) == "cute" {
+		imgEmbed := embed.NewEmbed().SetImage("https://media1.tenor.com/images/7052bc22327a608efe27f343ab6c0142/tenor.gif").MessageEmbed
+
+		s.ChannelMessageSendEmbed(m.ChannelID, imgEmbed)
+	}
+}
+
+func isAdmin(au *string) bool {
+	if *au == "385922547591675905" {
+		return true
+	}
+	return false
 }
